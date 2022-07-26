491fb9d1363e494dd1766242b59b0488bdd97475
Roman Sergeev <r2.sergeev@qa-academy.by>
2022-07-26 23:40:45 +0300

    lesson 8 паттерны

--- src/main/java/com/sergeev/application/actual/TestSpring.java
+++ src/main/java/com/sergeev/application/actual/TestSpring.java
@@ -6,8 +6,15 @@ public class TestSpring {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(firstMusicPlayer == secondMusicPlayer);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+
+//        musicPlayer.playMusicList();
         context.close();
     }
 }
